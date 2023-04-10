074fe154d7a63e9244d780587c57d1871e38dff5
Ovsclassic <okunoyevictor@gmail.com>
2023-04-10 21:54:34 +0100

    new packages installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
 import { Button } from "@mui/material";
+import { Add, Settings } from "@mui/icons-material";
 
 function App() {
   return (
     <div>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button
+        startIcon={<Settings />}
+        variant="contained"
+        color="secondary"
+        size="small"
+      >
+        Settings
+      </Button>
+      <Button
+        startIcon={<Add />}
+        variant="contained"
+        color="success"
+        size="small"
+      >
+        Add new post
+      </Button>
+      <Button variant="outlined" disabled>
+        Outlined
+      </Button>
+      {/*<Typography variant="h1" component="p">
+        It uses h1 style but it's a p tag
+  </Typography>*/}
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "skyblue", color: "#888", margin: 5 }}
+      >
+        My Unique Button
+      </Button>
     </div>
   );
 }
